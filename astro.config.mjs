1d8e9ca232bc04cad6f7f4f324fd4e2553c96e1f
hotape <xjpeterx@gmail.com>
2025-07-18 15:29:36 +0200

    Fix FreeEntry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+// astro.config.mjs
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        plugins: [
+            {
+                name: "fix-null-path-windows",
+                // se ejecuta antes de que Vite pida stat() del archivo
+                enforce: "pre",
+                resolveId(id) {
+                    if (
+                        id.startsWith("\0astro-entry:") &&
+                        id.endsWith("tsconfig.json")
+                    ) {
+                        return { id, external: true }; // lo marca externo y evita fs.stat
+                    }
+                },
+            },
+        ],
+    },
+});
